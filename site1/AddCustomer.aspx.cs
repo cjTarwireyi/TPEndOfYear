a093351427861d834e1387190549a9c5bbc26350
Cornelious <codzatazz@gmail.com>
2016-10-22 14:49:36 +0200

    Activated authentication on Add user Form

--- site1/AddCustomer.aspx.cs
+++ site1/AddCustomer.aspx.cs
@@ -7,11 +7,16 @@ using System.Web.UI.WebControls;
 
 public partial class site1_AddCustomer : System.Web.UI.Page
 {
+    private UserDTO userDto = new UserDTO();
+    
     protected void Page_Load(object sender, EventArgs e)
     {
         UserDTO userDtoUpdate = new UserDTO();
         userDtoUpdate = (UserDTO)Session["userUpdate"];
         Session.Remove("userUpdate");
+        userDto = (UserDTO)Session["userDto"];
+        if (userDto == null)
+            Response.Redirect("LoginPage.aspx");
     }
     protected void btnAdd_Click(object sender, EventArgs e)
     {
