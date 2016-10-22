68fc4a79aace868f1d8cc30a501374bf838e3f64
Cornelious <codzatazz@gmail.com>
2016-10-22 12:50:07 +0200

    initiated Brand domain

---
+++ App_Code/brand/BrandDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BrandDTO
+/// </summary>
+public class BrandDTO
+{
+	public BrandDTO()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
