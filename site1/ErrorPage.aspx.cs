be214a145a6b8ce55875d308402f48ba31848254
214068730 <siraajw19@gmail.com>
2016-10-29 15:46:31 +0200

    error page included

---
+++ site1/ErrorPage.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class site1_ErrorPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["ErrorMessage"] != null)
+            lblErrorDetails.Text = Request.QueryString["ErrorMessage"].Trim();
+    }
+}
