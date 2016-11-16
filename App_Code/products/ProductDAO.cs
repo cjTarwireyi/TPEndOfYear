ce2a4f33e96204cbf065d693de2cff1b98273177
214068730 <siraajw19@gmail.com>
2016-11-16 18:44:08 +0200

    Product DAO class udpdated

--- App_Code/products/ProductDAO.cs
+++ App_Code/products/ProductDAO.cs
@@ -12,81 +12,82 @@ using System.Web;
 public class ProductDAO
 {
     private SqlConnection con;
-	public ProductDAO()
-	{
+    public ProductDAO()
+    {
         con = new SqlConnection(ConfigurationManager.ConnectionStrings["AdminBookingConnectionString"].ConnectionString);
-	}
+    }
 
     public void saveProduct(Products product)
     {
-        //try
-        //{
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "insert into Products([ProductName], [ProductDescription], [Quantity],[Price], [Active],[DateArrived],[SupplierID])values('" + product.productName + "','" + product.productDescription + "','" + product.productQuantity + "','"+product.price+"','" +product.productStatus+ "','" +product.dateArrived+ "','"+product.productSupplierID+"')";
-            cmd.ExecuteNonQuery();
-            con.Close();
-        //}
-        //catch (Exception e)
-        //{
-        //}
-        //finally
-        //{
-        //    con.Close();
-        //}
+
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "insert into Products([ProductName], [ProductDescription], [Quantity],[Price], [Active],[DateArrived],[SupplierID])values('" + product.productName + "','" + product.productDescription + "','" + product.productQuantity + "','" + product.price + "','" + product.productStatus + "','" + product.dateArrived + "','" + product.productSupplierID + "')";
+        cmd.ExecuteNonQuery();
+        con.Close();
     }
 
 
     public Products makeProductDTO(SqlDataReader myDR)
     {
-        try
-        {
-            Products product = new Products();
-            product.productNumber = myDR.GetInt32(0);
-            product.productName = myDR.GetString(1);
-            product.productDescription = myDR.GetString(2);
-            product.price = myDR.GetDecimal(3);
-            product.productQuantity = myDR.GetInt32(4);
-            product.productStatus = myDR.GetBoolean(5);
-            product.dateArrived = myDR.GetDateTime(6);
-            product.productSupplierID = myDR.GetInt32(7);
-            return product;
-        }
-        catch(Exception e)
-        { 
-            return null;
-        }
+        Products product = new Products();
+        product.productNumber = myDR.GetInt32(0);
+        product.productName = myDR.GetString(1);
+        product.productDescription = myDR.GetString(2);
+        product.price = myDR.GetDecimal(3);
+        product.productQuantity = myDR.GetInt32(4);
+        product.productStatus = myDR.GetBoolean(5);
+        product.dateArrived = myDR.GetDateTime(6);
+        product.productSupplierID = myDR.GetInt32(7);
+        return product;
+
     }
 
     public Products getProduct(int id)
     {
-        try
-        {
-            con.Open();
-            String selectProduct = "select * from Products where Id =" + id + " ";
-            SqlCommand myComm = new SqlCommand(selectProduct, con);
-            SqlDataReader myDR;
-            myDR = myComm.ExecuteReader();
-            if (!myDR.Read())
-                return null;
-            Products updateProduct = makeProductDTO(myDR);
-            con.Close();
-            return updateProduct;
-        }
-        catch (Exception e)
-        {
+        con.Open();
+        String selectProduct = "select * from Products where Id =" + id + " ";
+        SqlCommand myComm = new SqlCommand(selectProduct, con);
+        SqlDataReader myDR;
+        myDR = myComm.ExecuteReader();
+        if (!myDR.Read())
             return null;
-        }
-        finally { con.Close(); }
-       
+        Products updateProduct = makeProductDTO(myDR);
+        con.Close();
+        return updateProduct;
     }
 
-    
+
     public SqlConnection connection()
     {
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["AdminBookingConnectionString"].ConnectionString);
         return con;
     }
 
+    public void itemBought(string productID,string quantity)
+    {
+        string oriQuantity="";
+        int newQuantity =0;
+        DataTable productQuantity = new DataTable();
+        con.Open();
+
+        string getQuantity = "select quantity from products where id ='" + productID + "' ";
+        SqlCommand cmd = new SqlCommand(getQuantity, con);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(productQuantity);
+        da.Dispose();
+        foreach (DataRow row in productQuantity.Rows)
+        {
+            oriQuantity = row["Quantity"].ToString();
+        }
+
+        newQuantity = Convert.ToInt32(oriQuantity) - Convert.ToInt32(quantity);
+
+        string updateQuery = "update products set quantity = '" + newQuantity + "' where id ='" + productID + "' ";
+        cmd = new SqlCommand(updateQuery, con);
+        cmd.ExecuteNonQuery();
+
+        con.Close();
+    }
 }
