8d6724084bb76d720fb4d3126fd37cbefd687b9c
Cornelious <codzatazz@gmail.com>
2016-11-27 11:12:19 +0200

    completed Insert test

--- UnitTests/Repos/UserTest.cs
+++ UnitTests/Repos/UserTest.cs
@@ -7,17 +7,27 @@ namespace UnitTests.Repos
     public class UserTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InsertUserTest()
         {
             
             IUser user = new UserDAO() ;
             UserDTO userDto = new UserDTO();
-              
-             
-            userDto.username="test";
-            userDto.password = "pass";
+            UserDTO addeUser,existingUser = new UserDTO();
+            string username = "test";
+            string password ="pass";
+            //Deleting test Data
+            existingUser = user.getUser(username, password);
+            user.Delete(existingUser.Id);
+
+            //Initalising new user
+            userDto.username=username;
+            userDto.password = password;
             userDto.userType =1;
             user.addUser(userDto);
+            //Searching for Added User
+            addeUser =   user.getUser(username, password);
+            Assert.AreEqual(username,addeUser.username.Trim());
+            Assert.AreEqual(password,addeUser.password.Trim() );
 
         }
     }
