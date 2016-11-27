7cfc2f0c571ed38137d87ad5dbfe7ccc95996423
Cornelious <codzatazz@gmail.com>
2016-11-27 21:45:07 +0200

    Created Employee Service

---
+++ BusineesLogic/Interface/IEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusineesLogic.Interface
+{
+   public interface IEmployee
+    {
+         void saveEmployee(EmployeeDTO emp);
+         EmployeeDTO makeTechDTO(SqlDataReader myDR);
+         EmployeeDTO getEmployee(int id);
+         DataTable populateGrid();
+
+    }
+}
