8acf2929ebfc18f7a9b4c1a0073a182e800552d1
Cornelious <codzatazz@gmail.com>
2016-11-27 07:04:44 +0200

    OrderDAO refactoring

--- BusineesLogic/domain/Orders/OrdersDAO.cs
+++ BusineesLogic/domain/Orders/OrdersDAO.cs
@@ -17,30 +17,7 @@ public class OrdersDAO : InterfaceOder
     {
         con = new SqlConnection(ConfigurationManager.ConnectionStrings["AdminBookingConnectionString"].ConnectionString);
     }
-    //    //public int AddOrder(OrderDTO model)
-    //    //{
-    //try
-    //        {
-    //            Oder1 order = new Oder1();
-    //            var time = DateTime.Now;
-    //            var orderCode = model.customerId + "-" + time;
-    //            order.custId = model.customerId;
-    //            order.employeeId = model.employeeId;
-    //            order.amount = model.amount;
-    //            order.payed = model.payed;
-    //            order.orderDate = time;
-    //            order.orderCode = orderCode;
-    //            db.Oder1s.InsertOnSubmit(order);
-    //            Oder1 orderfound = db.Oder1s.Where(t => t.orderCode == orderCode).Single();
-    //            return orderfound.orderId;
-    //        }
-    //       catch(Exception e) {
-    //            e.GetBaseException();
-    //            return new Int32();
-
-    //        }
-    //    /}
-
+     
     private int getOrder(string orderCode)
     {
 
