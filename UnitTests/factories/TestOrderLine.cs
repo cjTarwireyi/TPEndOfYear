661548a2be30bd35feaf922c7ee331a27b3aeb38
Cornelious <codzatazz@gmail.com>
2016-11-27 23:19:44 +0200

    created orderline factory test

---
+++ UnitTests/factories/TestOrderLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.factories;
+namespace UnitTests.factories
+{
+    [TestClass]
+    public class TestOrderLine
+    {
+        [TestMethod]
+        public void createOrderLine()
+        {
+            OrderLineDTO orderLine = OrderLineFactory.createOrderLine(1, 1, 20);
+
+            Assert.AreEqual(1, orderLine.productID);
+            Assert.AreEqual(1, orderLine.orderId);
+            Assert.AreEqual(20, orderLine.quantity);
+        }
+    }
+}
