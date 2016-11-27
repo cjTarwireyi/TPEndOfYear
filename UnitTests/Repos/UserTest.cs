f04a2a802533b74840fe27bcd804430621e18716
Cornelious <codzatazz@gmail.com>
2016-11-27 09:51:30 +0200

    User Repo test created

---
+++ UnitTests/Repos/UserTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.Interface;
+namespace UnitTests.Repos
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            IUser user = new UserDAO() ;
+            UserDTO userDto = new UserDTO();
+              
+             
+            userDto.username="test";
+            userDto.password = "pass";
+            userDto.userType =1;
+            user.addUser(userDto);
+
+        }
+    }
+}
