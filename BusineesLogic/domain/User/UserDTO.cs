b8c986d54ff0c552793e9cd20d9d7982356e864d
Cornelious <codzatazz@gmail.com>
2016-11-27 12:17:43 +0200

    created use Builder

--- BusineesLogic/domain/User/UserDTO.cs
+++ BusineesLogic/domain/User/UserDTO.cs
@@ -19,6 +19,30 @@ public class UserDTO
     
     public int userType { get; set; }
     public  UserTypeDTO userTypeDto  { get ; set; }
-     
+
+    public class UserBuilder
+    {
+        string userName;
+        string contact;
+        string email;
+        string password;
+        UserTypeDTO userTypeDto;
+        public UserBuilder buildUsername(string userName)
+        {
+            this.userName = userName;
+            return this;
+        }
+        public UserBuilder buildPassword(string pass)
+        {
+            this.password = pass;
+            return this;
+        }
+        public UserBuilder builduserType(UserTypeDTO userType)
+        {
+            this.userTypeDto = userType;
+            return this;
+        }
+    }
+
 
 }
