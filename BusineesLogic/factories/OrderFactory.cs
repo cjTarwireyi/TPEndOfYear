7192f0070f4f18e32ec6f48afb2b4f773df1c838
Cornelious <codzatazz@gmail.com>
2016-11-27 15:14:26 +0200

    Created Order Factory

---
+++ BusineesLogic/factories/OrderFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusineesLogic.factories;
+
+namespace BusineesLogic.factories
+{
+    public class OrderFactory
+    {
+
+        public static OrderDTO createOrder(int empId, int custId, Decimal amount, bool payed, List<Products> products)
+        {
+            return new OrderDTO.OrderBuilder()
+            .buildEmpId(empId)
+            .buildAmount(amount)
+            .buildPayed(payed)
+            .buildProducts(products)
+            .build();
+
+        }
+    }
+}
+
