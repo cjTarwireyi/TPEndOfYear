ce37a03875481d009fcbbdd120e34b8b9ad25757
214068730 <siraajw19@gmail.com>
2016-11-28 03:31:21 +0200

    testing

---
+++ UnitTests/factories/products/TestProducts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.factories
+{
+    [TestClass]
+    public class TestProducts
+    {
+        [TestMethod]
+        public void createProduct()
+        {
+            Products product = new Products.ProductsBuilder()
+            .prodName("Iphone")
+            .prodDescription("Sliver 16GB 6S")
+            .prodQuantity(5)
+            .prodPrice((decimal)12000.00)
+            .prodStatus(true)
+            .build();
+
+            Assert.AreEqual(product.productName, "Iphone");
+            Assert.IsTrue(product.productStatus);
+
+        }
+    }
+}
