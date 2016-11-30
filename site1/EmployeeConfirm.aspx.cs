664fd8f59c29824bde2ffbb9a33f7ae36b419549
214068730 <siraajw19@gmail.com>
2016-11-30 03:03:26 +0200

    server side updated

--- site1/EmployeeConfirm.aspx.cs
+++ site1/EmployeeConfirm.aspx.cs
@@ -7,30 +7,52 @@ using System.Web.UI.WebControls;
 
 public partial class site1_EmployeeConfirm : System.Web.UI.Page
 {
+    private UserDTO userDtoUpdate;
+    private UserDTO userDto;
+    private EmployeeDTO employeeDTO;
+    private EmployeeDAO employee = new EmployeeDAO();
     protected void Page_Load(object sender, EventArgs e)
     {
-        UserDTO userDtoUpdate = new UserDTO();
-        userDtoUpdate = (UserDTO)Session["userUpdate"];
-        Session.Remove("userUpdate");
-        EmployeeDTO employee = (EmployeeDTO)Session["EmployeeDTO"];
-
-        lblName.Text = employee.employeeName;
-        lblSurname.Text = employee.employeeSurname;
-        lblCellNumber.Text = employee.employeeCellNumber;
-        lblStreet.Text = employee.employeeStreetName;
-        lblSurbub.Text = employee.employeeSuburb;
-        lblPostalCode.Text = employee.employeePostalCode;
+        session();   
     }
+        
+        
     protected void btnConfirm_Click(object sender, EventArgs e)
     {
-        EmployeeDTO empDTO = (EmployeeDTO)Session["EmployeeDTO"];
-        EmployeeDAO employee = new EmployeeDAO();
-        employee.save(empDTO);
-        Response.Redirect("Employee.aspx");
+        try
+        {
+            employeeDTO = (EmployeeDTO)Session["EmployeeDTO"];
+            employee.save(employeeDTO);
+            Response.Redirect("Employee.aspx");
+        }
+        catch(Exception ex)
+        {
+            ExceptionRedirect(ex);
+        }
+
     }
 
     private void ExceptionRedirect(Exception ex)
     {
         Response.Redirect("ErrorPage.aspx?ErrorMessage=" + ex.Message.Replace('\n', ' '), false);
+    }
+
+    private void session()
+    {
+        userDtoUpdate = (UserDTO)Session["userUpdate"];
+        Session.Remove("userUpdate");
+        employeeDTO = (EmployeeDTO)Session["EmployeeDTO"];
+        userDtoUpdate = (UserDTO)Session["userUpdate"];
+        Session.Remove("userUpdate");
+        userDto = (UserDTO)Session["userDto"];
+        if (userDto == null)
+            Response.Redirect("LoginPage.aspx");
+        else
+           lblName.Text = employeeDTO.employeeName;
+           lblSurname.Text = employeeDTO.employeeSurname;
+           lblCellNumber.Text = employeeDTO.employeeCellNumber;
+           lblStreet.Text = employeeDTO.employeeStreetName;
+           lblSurbub.Text = employeeDTO.employeeSuburb;
+           lblPostalCode.Text = employeeDTO.employeePostalCode;
     }
 }
