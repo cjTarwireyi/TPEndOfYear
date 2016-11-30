b528268e45e11b8ea47c8c3c0b6e6f17158fb63f
Cornelious <codzatazz@gmail.com>
2016-11-30 21:09:16 +0200

    Insert test done

--- UnitTests/Repos/OrderLineTest.cs
+++ UnitTests/Repos/OrderLineTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using BusineesLogic.factories;
+using System.Collections.Generic;
 namespace UnitTests.Repos
 {
     [TestClass]
@@ -9,6 +10,11 @@ namespace UnitTests.Repos
         [TestMethod]
         public void TestMethod1()
         {
+            InterfaceOrderLine orderlineService = new OrderLineDAO() ;
+            OrderLineDTO orderLineDTO = OrderLineFactory.createOrderLine(1, 1, 2);
+            List<OrderLineDTO> lst = new List<OrderLineDTO>();
+            lst.Add(orderLineDTO);
+            orderlineService.AddOderLine(lst);
         }
     }
 }
