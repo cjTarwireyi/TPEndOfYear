98d0cb90e7731b57e64f1eb87e54e95811d601f0
214068730 <siraajw19@gmail.com>
2016-11-30 03:02:07 +0200

    server side updated

--- site1/AddCustomer.aspx.cs
+++ site1/AddCustomer.aspx.cs
@@ -7,51 +7,40 @@ using System.Web.UI.WebControls;
 
 public partial class site1_AddCustomer : System.Web.UI.Page
 {
-    private UserDTO userDto = new UserDTO();
-    
+    private UserDTO userDto;
+    private UserDTO userDtoUpdate;
     protected void Page_Load(object sender, EventArgs e)
     {
-        UserDTO userDtoUpdate = new UserDTO();
+        session();
+    }
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        string name = txtName.Text;
+        string surname = txtSurname.Text;
+        string cellNumber = txtCellNumber.Text;
+        string streetName = txtStreetName.Text;
+        string suburb = txtSuburb.Text;
+        string postal = txtPostalCode.Text;
+        string email = Email.Text;
+
+        CustomerDTO customer = new CustomerDTO.CustomerBuilder()
+            .custName(name)
+            .custSurname(surname)
+            .custCellNumber(cellNumber)
+            .custEmail(email)
+            .custAddress(streetName, suburb, postal)
+            .build();
+        Session["CustomerDTO"] = customer;
+        Server.Transfer("ConfirmCustomer.aspx", true);
+    }
+    private void session()
+    {
         userDtoUpdate = (UserDTO)Session["userUpdate"];
         Session.Remove("userUpdate");
         userDto = (UserDTO)Session["userDto"];
         if (userDto == null)
             Response.Redirect("LoginPage.aspx");
+        userDtoUpdate = (UserDTO)Session["userUpdate"];
+        Session.Remove("userUpdate");
     }
-    protected void btnAdd_Click(object sender, EventArgs e)
-    {
-        try
-        {
-            string name = txtName.Text;
-            string surname = txtSurname.Text;
-            string cellNumber = txtCellNumber.Text;
-            string streetName = txtStreetName.Text;
-            string suburb = txtSuburb.Text;
-            string postal = txtPostalCode.Text;
-            string email = Email.Text;
-
-            CustomerDTO customer = new CustomerDTO.CustomerBuilder()
-                .custName(name)
-                .custSurname(surname)
-                .custCellNumber(cellNumber)
-                .custEmail(email)
-                .custAddress(streetName, suburb, postal)
-                .build();
-            Session["CustomerDTO"] = customer;
-        }
-        catch (Exception ex)
-        {
-
-        }
-        finally
-        {
-            //Response.Redirect("../site1/ConfirmCustomer.aspx");
-            Server.Transfer("ConfirmCustomer.aspx", true);
-        }
-
-
-    }
-
-    
-
 }
