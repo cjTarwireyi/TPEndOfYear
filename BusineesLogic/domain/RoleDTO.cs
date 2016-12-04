2dcd43c19ff13259ba54d555febd756fe8851aff
Cornelious <codzatazz@gmail.com>
2016-12-04 11:43:46 +0200

    created role name property

--- BusineesLogic/domain/RoleDTO.cs
+++ BusineesLogic/domain/RoleDTO.cs
@@ -11,20 +11,26 @@ public class RoleDTO
     public RoleDTO(RoleBuilder roleBuilder)
     {
         this.roleId = roleBuilder.roleId;
+        this.roleName = roleBuilder.roleName;
         this.roleDescription = roleBuilder.roleDescription;
     }
     public int roleId { get; set; }
-    //public int userTypeId { get; set; }
+    public string roleName { get; set; }
     public string roleDescription { get; set; }
     public class RoleBuilder
     {
         public int roleId;
-        //public string usertypeID;
+        public string roleName;
         public string roleDescription;
 
         public RoleBuilder buildRoleID(int roleId)
         {
             this.roleId = roleId;
+            return this;
+        }
+        public RoleBuilder buildRoleName(string roleName)
+        {
+            this.roleName = roleName;
             return this;
         }
         public RoleBuilder buildroleDescription(string roleDescription)
@@ -35,6 +41,7 @@ public class RoleDTO
         public RoleBuilder copy(RoleDTO roleDTO)
         {
             this.roleId = roleDTO.roleId;
+            this.roleName = roleDTO.roleName;
             this.roleDescription = roleDTO.roleDescription;
             return this;
         }
