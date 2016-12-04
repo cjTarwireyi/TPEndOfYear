3537d0ec8b35216a35d7c77d0faf43897be1ec11
Cornelious <codzatazz@gmail.com>
2016-12-04 11:53:08 +0200

    implemented interface

--- BusineesLogic/Services/RoleDAO.cs
+++ BusineesLogic/Services/RoleDAO.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusineesLogic.Interface;
 
 namespace BusineesLogic.services
 {
-   public class RoleDAO
+   public class RoleDAO:IRoleService
     {
+
+        public bool addRole(RoleDTO role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool updateRole(RoleDTO role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteRole(int roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoleDTO findRole(int roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RoleDTO> getAllRoles()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
