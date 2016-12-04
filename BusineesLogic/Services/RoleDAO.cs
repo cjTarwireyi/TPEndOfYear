6e237ffe907bb46cd81bebbf7b57a4c6eeccf7bd
Cornelious <codzatazz@gmail.com>
2016-12-04 11:55:20 +0200

    created connection string

--- BusineesLogic/Services/RoleDAO.cs
+++ BusineesLogic/Services/RoleDAO.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusineesLogic.Interface;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace BusineesLogic.services
 {
    public class RoleDAO:IRoleService
     {
+       private SqlConnection con;
+       public RoleDAO()
+    {
+        con = new SqlConnection(ConfigurationManager.ConnectionStrings["AdminBookingConnectionString"].ConnectionString);
+    }
 
         public bool addRole(RoleDTO role)
         {
