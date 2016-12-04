ba8d9e21209acfbb1e433b66bff67d2362461bcf
Cornelious <codzatazz@gmail.com>
2016-12-04 10:54:44 +0200

    created role builder

--- BusineesLogic/domain/RoleDTO.cs
+++ BusineesLogic/domain/RoleDTO.cs
@@ -8,9 +8,40 @@ using System.Web;
 /// </summary>
 public class RoleDTO
 {
-
+    public RoleDTO(RoleBuilder roleBuilder)
+    {
+        this.roleId = roleBuilder.roleId;
+        this.roleDescription = roleBuilder.roleDescription;
+    }
     public int roleId { get; set; }
-    public int userTypeId { get; set; }
+    //public int userTypeId { get; set; }
     public string roleDescription { get; set; }
+    public class RoleBuilder
+    {
+        public int roleId;
+        //public string usertypeID;
+        public string roleDescription;
+
+        public RoleBuilder buildRoleID(int roleId)
+        {
+            this.roleId = roleId;
+            return this;
+        }
+        public RoleBuilder buildroleDescription(string roleDescription)
+        {
+            this.roleDescription = roleDescription;
+            return this;
+        }
+        public RoleBuilder copy(RoleDTO roleDTO)
+        {
+            this.roleId = roleDTO.roleId;
+            this.roleDescription = roleDTO.roleDescription;
+            return this;
+        }
+        public RoleDTO build()
+        {
+            return new RoleDTO(this);
+        }
+    }
 }
 	 
