0208f5dfb0978bd4bb85eb044b18f17fdcc3e11b
Cornelious <codzatazz@gmail.com>
2016-12-04 12:59:27 +0200

    removed role id on role creation

--- UnitTests/domain/TestRole.cs
+++ UnitTests/domain/TestRole.cs
@@ -11,9 +11,9 @@ namespace UnitTests.factories
         public void testRole()
         {
             //Create
-            RoleDTO roleDto = RoleFactory.createRole(1, "full control","Delete,Update");
+            RoleDTO roleDto = RoleFactory.createRole( "full control","Delete,Update");
             Assert.AreEqual("Delete,Update", roleDto.roleDescription);
-            Assert.AreEqual(1, roleDto.roleId);
+            
             Assert.AreEqual("full control", roleDto.roleName);
 
             //Update
@@ -21,7 +21,7 @@ namespace UnitTests.factories
                 .copy(roleDto)
                 .buildroleDescription("view only")
                 .build();
-            Assert.AreEqual(1, updateRole.roleId);
+              
             Assert.AreNotEqual("Delete,Update", updateRole.roleDescription);
             Assert.AreEqual("full control", updateRole.roleName);
         }
