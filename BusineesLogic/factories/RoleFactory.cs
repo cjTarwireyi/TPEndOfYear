b153b0fa9d7b293b043bc2dd1435de54b909b6c5
Cornelious <codzatazz@gmail.com>
2016-12-04 10:58:42 +0200

    created role factory

---
+++ BusineesLogic/factories/RoleFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusineesLogic.factories
+{
+   public class RoleFactory
+    {
+       public static RoleDTO createRole(int roleId, string roleDesc)
+       {
+           return new RoleDTO.RoleBuilder()
+           .buildRoleID(roleId)
+           .buildroleDescription(roleDesc)
+           .build();
+       
+       }
+    }
+}
