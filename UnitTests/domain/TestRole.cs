de236e3042ed97b6735340df5ec30cb184a640f5
Cornelious <codzatazz@gmail.com>
2016-12-04 11:16:33 +0200

    created role test

---
+++ UnitTests/domain/TestRole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.factories;
+
+namespace UnitTests.factories
+{
+    [TestClass]
+    public class TestRole
+    {
+        [TestMethod]
+        public void testRole()
+        {
+            //Create
+            RoleDTO roleDto = RoleFactory.createRole(1, "Delete,Update");
+            Assert.AreEqual("Delete,Update", roleDto.roleDescription);
+            Assert.AreEqual(1, roleDto.roleId);
+
+            //Update
+            RoleDTO updateRole = new RoleDTO.RoleBuilder()
+                .copy(roleDto)
+                .buildroleDescription("view only")
+                .build();
+            Assert.AreEqual(1, updateRole.roleId);
+            Assert.AreNotEqual("Delete,Update", updateRole.roleDescription);
+        }
+         
+
+    }
+}
