a4e3306e5b58cf30602383695d0e73485ab9c09b
Cornelious <codzatazz@gmail.com>
2016-12-04 11:44:00 +0200

    test role name

--- BusineesLogic/factories/RoleFactory.cs
+++ BusineesLogic/factories/RoleFactory.cs
@@ -8,10 +8,11 @@ namespace BusineesLogic.factories
 {
    public class RoleFactory
     {
-       public static RoleDTO createRole(int roleId, string roleDesc)
+       public static RoleDTO createRole(int roleId,string roleName, string roleDesc)
        {
            return new RoleDTO.RoleBuilder()
            .buildRoleID(roleId)
+           .buildRoleName(roleName)
            .buildroleDescription(roleDesc)
            .build();
        
