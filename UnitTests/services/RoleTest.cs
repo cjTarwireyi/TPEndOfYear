ff043824433551ac3cc62daa92fca8a689f2e9d3
Cornelious <codzatazz@gmail.com>
2016-12-04 12:12:13 +0200

    tested find by Id

--- UnitTests/services/RoleTest.cs
+++ UnitTests/services/RoleTest.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using BusineesLogic.Interface;
+using BusineesLogic.factories;
+using BusineesLogic.services;
+using BusineesLogic.domain;
 namespace UnitTests.services
 {
     [TestClass]
     public class RoleTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestRole()
         {
+            IRoleService service = new RoleDAO();
+
+            RoleDTO role = service.findRole(1);
+            Assert.IsNotNull(role);
         }
     }
 }
