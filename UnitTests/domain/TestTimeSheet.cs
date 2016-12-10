11bcea56ef156ecbf9df335a1ab34310d9fffb24
Cornelious <codzatazz@gmail.com>
2016-12-10 19:09:47 +0200

    created temesheet test

---
+++ UnitTests/domain/TestTimeSheet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.domain
+{
+    [TestClass]
+    public class TestTimeSheet
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
