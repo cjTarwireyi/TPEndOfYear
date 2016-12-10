0f16adbc8bb9b0d1bcdfa2f888e0343070319616
Cornelious <codzatazz@gmail.com>
2016-12-10 20:02:08 +0200

    update

--- BusineesLogic/Services/TimeSheetDAO.cs
+++ BusineesLogic/Services/TimeSheetDAO.cs
@@ -4,10 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusineesLogic.Interface;
+using BusineesLogic.domain;
 
 namespace BusineesLogic.services
 {
    public class TimeSheetDAO:ITimeSheet
     {
+        public TimeSheetDTO getTimeSheet(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ifTimeSheetExis(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getTimesheetId(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool addTimeSheet(domain.TimeSheetDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTimeSheet(TimeSheetDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
