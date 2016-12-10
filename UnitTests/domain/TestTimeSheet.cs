e365da51b429e9b04a003d570646866e141d4270
Cornelious <codzatazz@gmail.com>
2016-12-10 19:28:19 +0200

    tested update

--- UnitTests/domain/TestTimeSheet.cs
+++ UnitTests/domain/TestTimeSheet.cs
@@ -11,10 +11,21 @@ namespace UnitTests.domain
         [TestMethod]
         public void TestTimeSheetDTO()
         {
+            //CREATE TEST
             TimeSheetDTO timesheetDto = TimeSheetFactory.createTimeSheet(14, 5, 12);
             Assert.AreEqual(14, timesheetDto.day);
             Assert.AreEqual(5, timesheetDto.hour);
             Assert.AreEqual(12, timesheetDto.minutes);
+
+            //UPDATE TEST
+            TimeSheetDTO update = new TimeSheetDTO.TimeSheetBuilder()
+                .copy(timesheetDto)
+                .buildDay(14)
+                .build();
+            Assert.AreEqual(14, update.day);
+            Assert.AreEqual(timesheetDto.hour, timesheetDto.hour);
+            Assert.AreEqual(timesheetDto.minutes, timesheetDto.minutes);
+
         }
     }
 }
