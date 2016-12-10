318f4f4501c4699ab1c41b1d497265389be678ca
Cornelious <codzatazz@gmail.com>
2016-12-10 19:07:35 +0200

    implemented timesheet factory

--- BusineesLogic/factories/TimeSheetFactory.cs
+++ BusineesLogic/factories/TimeSheetFactory.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusineesLogic.domain;
 
 namespace BusineesLogic.factories
 {
-    class TimeSheetFactory
+   public class TimeSheetFactory
     {
+       public static TimeSheetDTO createTimeSheet(int id, int day, int hour, int minutes)
+       {
+           return new TimeSheetDTO.TimeSheetBuilder()
+           .buildId(id)
+           .buildDay(day)
+           .buildHour(hour)
+           .buildMinutes(minutes)
+           .build();
+       }
+        
     }
 }
