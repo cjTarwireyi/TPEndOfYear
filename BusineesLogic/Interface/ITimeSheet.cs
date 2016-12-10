15cf44a7d490db57666589a9ca4ef9ddca2f17b3
Cornelious <codzatazz@gmail.com>
2016-12-10 19:36:03 +0200

    created methods

--- BusineesLogic/Interface/ITimeSheet.cs
+++ BusineesLogic/Interface/ITimeSheet.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusineesLogic.domain ;
 
 namespace BusineesLogic.Interface
 {
     public interface ITimeSheet
     {
+        TimeSheetDTO getTimeSheet(string username, string password);
+        bool ifTimeSheetExis(string username);
+        int getTimesheetId(string username);
+        bool addTimeSheet(TimeSheetDTO model);
+        bool UpdateTimeSheet(TimeSheetDTO model);
+        bool Delete(int id);
     }
 }
