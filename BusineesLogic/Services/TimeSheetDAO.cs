e5641d11c7e100ae0951e44046e0ff8230967ec2
Cornelious <codzatazz@gmail.com>
2016-12-11 10:18:03 +0200

    methods implemented

--- BusineesLogic/Services/TimeSheetDAO.cs
+++ BusineesLogic/Services/TimeSheetDAO.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusineesLogic.Interface;
+using System.Data.SqlClient;
+using System.Configuration;
+using BusineesLogic.factories;
+using System.Data;
 using BusineesLogic.domain;
 
 namespace BusineesLogic.services
 {
    public class TimeSheetDAO:ITimeSheet
     {
+       private SqlConnection con;
+       public TimeSheetDAO()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["AdminBookingConnectionString"].ConnectionString);
+        }
 
         public TimeSheetDTO getTimeSheet(DateTime date)
         {
@@ -28,17 +39,83 @@ namespace BusineesLogic.services
 
         public bool addTimeSheet(TimeSheetDTO model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var Datetime = DateTime.Now;
+
+                con.Open();
+                string insertQuery = "INSERT INTO TimeSheet (DateWorked,HourIn,HourOut,EmployeeID) VALUES ('" + model.date + "','" + model.hourIn + "','" + model.hourOut + "','" + model.employeeId + "')";
+                SqlCommand cmd = new SqlCommand(insertQuery, con);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
 
         public bool UpdateTimeSheet(TimeSheetDTO model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                con.Open();
+                string updateQuery = "Update TimeSheet Set DateWorked= '" + model.date + "',HourIn='" + model.hourIn + "',HourOut='" + model.hourOut + "',EmployeeID='" + model.employeeId + "' Where Id ='" + model.id+ "'";
+                SqlCommand cmd = new SqlCommand(updateQuery, con);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
 
         public bool Delete(int id)
         {
             throw new NotImplementedException();
+        }
+
+
+        public TimeSheetDTO getLastReocrd()
+        {
+            try
+            {
+                if (con.State.ToString() == "Open")
+                    con.Close();
+                TimeSheetDTO timeSheet;
+                con.Open();
+                String selectTimeSheet = "SELECT TOP 1 * FROM  TimeSheet Order by Id DESC ";
+                SqlCommand myComm = new SqlCommand(selectTimeSheet, con);
+                SqlDataReader reader;
+                reader = myComm.ExecuteReader();
+                if (!reader.Read())
+                {
+                    timeSheet = null;
+                }
+                else
+                {
+                    timeSheet = new TimeSheetDTO.TimeSheetBuilder()
+                                       .buildId(reader.GetInt32(0))
+                                       .buildDate(reader.GetDateTime(1))
+                                       .buildHourIn(reader.GetInt32(2))
+                                       .buildHourOut(reader.GetInt32(3))
+                                       .buildEmployeeID(reader.GetInt32(4))
+                                       .build();
+
+                }
+                con.Close();
+                return timeSheet;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+
         }
     }
 }
