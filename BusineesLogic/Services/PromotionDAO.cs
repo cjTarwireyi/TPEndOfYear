cfa64662b9ff45d7c174472d31a162ac8ff66a27
Cornelious <codzatazz@gmail.com>
2016-12-11 15:23:20 +0200

    Crud test implemented

--- BusineesLogic/Services/PromotionDAO.cs
+++ BusineesLogic/Services/PromotionDAO.cs
@@ -62,17 +62,76 @@ namespace BusineesLogic.services
 
         public bool Update(PromotionDTO model)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                con.Open();
+                string updateQuery = "Update Promotion Set DiscountPercent= '" + model.promotionDetails.discountPercent + "',StartDate='" + model.promotionDetails.startDate + "',EndDate='" + model.promotionDetails.endDate + "',DateCreated='" + model.dateCreated + "' Where Id ='" + model.id + "'";
+                SqlCommand cmd = new SqlCommand(updateQuery, con);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
 
         public PromotionDTO getLastReocrd()
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                if (con.State.ToString() == "Open")
+                    con.Close();
+                PromotionDTO promotion;
+                PromotionDetailsDTO promotionDetails;
+                con.Open();
+                String selectTimeSheet = "SELECT TOP 1 * FROM  Promotion Order by Id DESC ";
+                SqlCommand myComm = new SqlCommand(selectTimeSheet, con);
+                SqlDataReader reader;
+                reader = myComm.ExecuteReader();
+                if (!reader.Read())
+                {
+                    promotion = null;
+                }
+                else
+                {
+                    promotionDetails = new PromotionDetailsDTO.PromotionDetailsBuilder()
+                                            .buildDiscountPercent(reader.GetDecimal(1))
+                                            .buildStartDate(reader.GetDateTime(2))
+                                            .buildEndDate(reader.GetDateTime(3))
+                                            .build();
+                    promotion = new PromotionDTO.PromotionBuilder()
+                                       .buildId(reader.GetInt32(0))
+                                       .buildPromotionDetails(promotionDetails)   
+                                       .buildDateCreated(reader.GetDateTime(4))                                       
+                                       .build();
+
+                }
+                con.Close();
+                return promotion;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                con.Open();
+                string deleteQuery = "DELETE FROM Promotion WHERE  Id ='" + id + "'";
+                SqlCommand cmd = new SqlCommand(deleteQuery, con);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
