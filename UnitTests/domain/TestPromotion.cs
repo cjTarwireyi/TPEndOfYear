78f37c57cb45c3f4324071e76d3d4170dc6de6d7
Cornelious <codzatazz@gmail.com>
2016-12-11 13:47:42 +0200

    created test promotion

---
+++ UnitTests/domain/TestPromotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.factories;
+using BusineesLogic.domain;
+
+namespace UnitTests.domain
+{
+    [TestClass]
+    public class TestPromotion
+    {
+        [TestMethod]
+        public void TestCreatePromotion()
+        {
+            PromotionDetailsDTO createDetails = PromotionDetailsFactory.createPromotionDetails(5, DateTime.Parse("1/2/2016 12:00:00 PM"), DateTime.Parse("1/2/2016 12:00:00 PM"));
+            PromotionDTO createDto = PromotionFactory.createPromotion(1, createDetails, DateTime.Now);
+            Assert.AreEqual(1, createDto.productId);
+            Assert.AreEqual(DateTime.Parse("1/2/2016 12:00:00 PM"), createDto.promotionDetails.endDate);
+            Assert.AreEqual(5, createDto.promotionDetails.discountPercent);
+        }
+    }
+}
