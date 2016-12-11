9e369a127f79369d841e751f1c5ae78acac5d029
Cornelious <codzatazz@gmail.com>
2016-12-11 11:14:53 +0200

    create Builder

--- BusineesLogic/domain/PromotionDTO.cs
+++ BusineesLogic/domain/PromotionDTO.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace BusineesLogic.domain
 {
-    class PromotionDTO
+    public class PromotionDTO
     {
+        public int id { get; set; }
+        public int productId { get; set; }
+        public decimal discountPercent { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public DateTime dateCreated { get; set; }
+
+        public class PromotionBuilder
+        {
+            public int id;
+            public int productId;
+            public decimal discountPercent;
+            public DateTime startDate;
+            public DateTime endDate;
+            public DateTime createdDate;
+
+            public PromotionBuilder buildId(int id)
+            {
+                this.id = id;
+                return this;
+            }
+            public PromotionBuilder BuildProductId(int productId)
+            {
+                this.productId = productId;
+                return this;
+            }
+            public PromotionBuilder buildDiscountPercent(decimal discountPercent)
+            {
+                this.discountPercent = discountPercent;
+                return this;
+            }
+            public PromotionBuilder buildStartDate(DateTime startDate)
+            {
+                this.startDate = startDate;
+                return this;
+            }
+            public PromotionBuilder buildEndDate(DateTime endDate)
+            {
+                this.endDate = endDate;
+                return this;
+            }
+            public PromotionBuilder buildDateCreated(DateTime createdDate)
+            {
+                this.createdDate = createdDate;
+                return this;
+            }
+            public PromotionBuilder copy(PromotionDTO promotionDTO)
+            {
+                this.id = promotionDTO.id;
+                this.productId = promotionDTO.productId;
+                this.discountPercent = promotionDTO.discountPercent;
+                this.startDate = promotionDTO.startDate;
+                this.endDate = promotionDTO.endDate;
+                this.createdDate = promotionDTO.dateCreated;
+                return this;
+            }
+        }
     }
 }
