354d4cddae5bd7e546295a6567bd5bcd24968ce7
Cornelious <codzatazz@gmail.com>
2016-12-11 11:45:04 +0200

    created factoy promotion

---
+++ BusineesLogic/factories/PromotionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusineesLogic.domain;
+
+namespace BusineesLogic.factories
+{
+    public class PromotionFactory
+    {
+        public static PromotionDTO createRole( int productID, PromotionDetailsDTO promotionDetails,DateTime dateCreated)
+        {
+            return new PromotionDTO.PromotionBuilder()
+
+             
+            .BuildProductId(productID)
+            .buildPromotionDetails(promotionDetails)
+            .buildDateCreated(dateCreated)
+            .build();
+
+        }
+    }
+}
