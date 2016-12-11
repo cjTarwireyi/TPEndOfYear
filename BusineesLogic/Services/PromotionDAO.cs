e3bd95ea807bf5382a2dc88024e8462fa6ae7cb2
Cornelious <codzatazz@gmail.com>
2016-12-11 13:58:39 +0200

    created promotion DAO

---
+++ BusineesLogic/Services/PromotionDAO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusineesLogic.Interface;
+using System.Data.SqlClient;
+using System.Configuration;
+using BusineesLogic.factories;
+using System.Data;
+using BusineesLogic.domain;
+
+
+namespace BusineesLogic.services
+{
+    public class PromotionDAO:IPromotion
+    {
+        public PromotionDTO getTimeSheet(System.DateTime date)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ifTimeSheetExis(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int getTimesheetId(System.DateTime date)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool addTimeSheet(PromotionDTO model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateTimeSheet(PromotionDTO model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PromotionDTO getLastReocrd()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
