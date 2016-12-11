22c8687416c35edc33fa26f8c5ceef1368fdca95
Cornelious <codzatazz@gmail.com>
2016-12-11 13:58:56 +0200

    created promotion interface

---
+++ BusineesLogic/Interface/IPromotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusineesLogic.domain;
+
+namespace BusineesLogic.Interface
+{
+  public  interface IPromotion
+    {
+        PromotionDTO getTimeSheet(DateTime date);
+        bool ifTimeSheetExis(string id);
+        int getTimesheetId(DateTime date);
+        bool addTimeSheet(PromotionDTO model);
+        bool UpdateTimeSheet(PromotionDTO model);
+        PromotionDTO getLastReocrd();
+
+        bool Delete(int id);
+    }
+}
