38d1d8d51407437f2d8c186bc708fe4f3d5406f0
yongzz <yongz.ngwenduna@gmail.com>
2016-12-12 16:56:09 +0200

    create tested

--- UnitTests/domain/BrandTest.cs
+++ UnitTests/domain/BrandTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.factories;
+using BusineesLogic.domain;
 
 namespace UnitTests.domain
 {
@@ -9,6 +11,12 @@ namespace UnitTests.domain
         [TestMethod]
         public void TestMethod1()
         {
+            //CREATE
+            BrandDTO createBrand = BrandFactory.createBrand("test", "test", DateTime.Parse("1/1/1900 12:00:00 AM"),true);
+            Assert.AreEqual("test", createBrand.brandName);
+            Assert.AreEqual("test", createBrand.brandDescription);
+            Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), createBrand.dateCreated);
+            Assert.AreEqual(true, createBrand.active);
         }
     }
 }
