22f6d192ada188ed36032510f66d0c8b3eec7493
yongzz <yongz.ngwenduna@gmail.com>
2016-12-12 17:06:17 +0200

    implemented interface

--- BusineesLogic/Services/BrandDAO.cs
+++ BusineesLogic/Services/BrandDAO.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusineesLogic.Interface;
 
 namespace BusineesLogic.services
 {
-    public class BrandDAO
+    public class BrandDAO:IBrand
     {
+        public List<domain.BrandDTO> getAllBrands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public domain.BrandDTO save(domain.BrandDTO brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public domain.BrandDTO update(domain.BrandDTO brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public domain.BrandDTO delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public domain.BrandDTO findBrand(long id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
