f5226c9cddefa596e6e2012bd9320ab472b23746
cjTarwireyi <codzatazz@gmail.com>
2016-12-15 00:43:09 +0200

    test category create and update

---
+++ UnitTests/domain/TestCategory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.factories;
+using BusineesLogic.domain;
+namespace UnitTests.domain
+{
+    [TestClass]
+    public class TestCategory
+    {
+        [TestMethod]
+        public void TestCategoryDTO()
+        {
+            //CREATE
+            CategoryDTO createCategory =  CategoryFactory.create("test", "test", DateTime.Parse("1/1/1900 12:00:00 AM"));
+            Assert.AreEqual("test", createCategory.name);
+            Assert.AreEqual("test", createCategory.description);
+            Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), createCategory.dateCreated);
+
+
+            //UPDATE
+            CategoryDTO updateCategory = new CategoryDTO.CategoryBuilder()
+            .copy(createCategory)
+            .buildDescription("update category")
+            .build();
+            Assert.AreEqual("update category", updateCategory.description);
+            Assert.AreEqual(createCategory.name, updateCategory.name);
+        }
+    }
+}
