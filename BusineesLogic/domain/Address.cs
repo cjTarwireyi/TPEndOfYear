9802857e3978ca2902d9ef46768213323b7e2a26
Yongzz <yongz.ngwenduna@gmail.com>
2016-12-14 02:30:01 +0200

    DTO updated

--- BusineesLogic/domain/Address.cs
+++ BusineesLogic/domain/Address.cs
@@ -12,7 +12,54 @@ public class Address
 	{
 		
 	}
-    public String streetName { set; get; }
-    public String suburb { set; get; }
-    public String postalCode { set; get; }
+
+    public Address(AddressBuilder address)
+    {
+        this.suburb = address.suburb;
+        this.streetName = address.streetName;
+        this.postalCode = address.postalCode;
+        
+    }
+
+    public string streetName;
+    public string suburb;
+    public string postalCode;
+
+     public class AddressBuilder
+     {
+         public string streetName;
+         public string suburb;
+         public string postalCode;
+
+         public AddressBuilder buildSuburb(string suburb)
+         {
+             this.suburb = suburb;
+             return this;
+         }
+
+         public AddressBuilder buildStreet(string streetName)
+         {
+             this.streetName = streetName;
+             return this;
+         }
+
+         public AddressBuilder buildPostalCode(string postalCode)
+         {
+             this.postalCode = postalCode;
+             return this;
+         }
+
+         public AddressBuilder buildPostalCode(Address address)
+         {
+             this.suburb = address.suburb;
+             this.streetName = address.streetName;
+             this.postalCode = address.postalCode;
+             return this;
+         }
+
+         public Address build()
+         {
+             return new Address(this);
+         }
+     }
 }
