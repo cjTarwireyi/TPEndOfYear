a3d624a2f44bff3cecd8afa2cfafcd91babb1639
Yongzz <yongz.ngwenduna@gmail.com>
2016-12-14 02:30:21 +0200

    DTO updated

--- BusineesLogic/domain/ReportDTO.cs
+++ BusineesLogic/domain/ReportDTO.cs
@@ -8,5 +8,59 @@ namespace BusineesLogic.domain
 {
     class ReportDTO
     {
+        string reportName { get; set; }
+        DateTime dateGenerated { get; set; }
+        string reportContent { get; set; }
+
+        private ReportDTO() { }
+
+        public ReportDTO(ReportBuilder report)
+        {
+            this.reportName = report.reportName;
+            this.dateGenerated = report.dateGenerated;
+            this.reportContent = report.reportContent;
+
+        }
+
+        public class ReportBuilder
+        {
+            public string reportName;
+            public DateTime dateGenerated;
+            public string reportContent;
+
+            public ReportBuilder buildReportName(string reportName)
+            {
+                this.reportName = reportName;
+                return this;
+            }
+
+            public ReportBuilder buildDateGenerated(DateTime dateGenerated)
+            {
+                this.dateGenerated = dateGenerated;
+                return this;
+            }
+
+            public ReportBuilder buildreportContent(string reportContent)
+            {
+                this.reportContent = reportContent;
+                return this;
+            }
+
+
+            public ReportBuilder copy(ReportDTO report)
+            {
+                this.reportName = report.reportName;
+                this.dateGenerated = report.dateGenerated;
+                this.reportContent = report.reportContent;
+                return this;
+            }
+
+
+            public ReportDTO build()
+            {
+                return new ReportDTO(this);
+            }
+
+        }
     }
 }
