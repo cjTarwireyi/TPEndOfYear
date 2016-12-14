301797d8982afc9b22d33f5bd805bd6708b4421a
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 22:38:06 +0200

    implemented factory metd=hod

--- BusineesLogic/factories/TransactionFactory.cs
+++ BusineesLogic/factories/TransactionFactory.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusineesLogic.domain;
 
 namespace BusineesLogic.factories
 {
-    class TransactionFactory
+   public class TransactionFactory
     {
+       public static TransactionDTO create(string code, decimal amount, DateTime dateModified)
+       {
+           return new TransactionDTO.TransactionBuilder()
+
+           .buildCode(code)
+           .buildAmount(amount)
+           .buildDateModified(dateModified)
+           .build();
+
+       }
     }
 }
