6fd1180d10007d3793bc996ecec4ffbec7437a90
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 22:15:27 +0200

    update test done

--- UnitTests/domain/TestUserAnalysis.cs
+++ UnitTests/domain/TestUserAnalysis.cs
@@ -13,10 +13,19 @@ namespace UnitTests.domain
             //CREATE TEST
             DateTime date = DateTime.Parse("1/1/1900 12:00:00 AM");
 
-            UserAnalysisDTO ua = UserAnalysisFactory.createAnalysis(1,"Sold The Most", date);
-            Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), ua.dateModified);
-            Assert.AreEqual(1, ua.userId);
-            Assert.AreEqual("Sold The Most", ua.description);
+            UserAnalysisDTO userAnalysis = UserAnalysisFactory.createAnalysis(1,"Sold The Most", date);
+            Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), userAnalysis.dateModified);
+            Assert.AreEqual(1, userAnalysis.userId);
+            Assert.AreEqual("Sold The Most", userAnalysis.description);
+
+            //TEST UPDATE
+            UserAnalysisDTO update = new UserAnalysisDTO.UserAnalysisBuilder()
+                .copy(userAnalysis)
+                .buildUserId(5)
+                .build();
+            Assert.AreEqual(5, update.userId);
+            Assert.AreEqual(update.description, userAnalysis.description);
+            Assert.AreEqual(update.dateModified, userAnalysis.dateModified);
 
         }
     }
