f5cf953bf7931461c7d011a0da4b959c0af892d0
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 22:43:48 +0200

    create tested

---
+++ UnitTests/domain/testTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.factories;
+using BusineesLogic.domain;
+
+namespace UnitTests.domain
+{
+    [TestClass]
+    public class TestTransaction
+    {
+        [TestMethod]
+        public void TestTransactionDTO()
+        {
+            //CREATE TEST
+            DateTime date = DateTime.Parse("1/1/1900 12:00:00 AM");
+
+            TransactionDTO transaction = TransactionFactory.create("123", 200, date);
+            Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), transaction.dateModified);
+            Assert.AreEqual(200, transaction.amount);
+            Assert.AreEqual("123", transaction.code);
+        }
+    }
+}
