051b7115ca66f99e1ca41a015b49715c31e155c2
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 21:22:47 +0200

    implemented leave builder

--- BusineesLogic/domain/LeaveDTO.cs
+++ BusineesLogic/domain/LeaveDTO.cs
@@ -8,6 +8,13 @@ namespace BusineesLogic.domain
 {
     public class LeaveDTO
     {
+        public LeaveDTO(LeaveBuilder leaveBuilder)
+        {
+            this.Id = leaveBuilder.Id;
+            this.leaveTitle = leaveBuilder.leaveTitle;
+            this.description = leaveBuilder.description;
+            this.dateModified = leaveBuilder.dateModified;
+        }
         public int Id{get; set;}
         public string leaveTitle{get;set;}
         public string description{get; set;}
@@ -15,6 +22,42 @@ namespace BusineesLogic.domain
     }
     public class LeaveBuilder
     {
-
+        public int Id;
+        public string leaveTitle;
+        public string description;
+        public DateTime dateModified;
+        public LeaveBuilder buildId(int id)
+        {
+            this.Id = id;
+            return this;
+        }
+        public LeaveBuilder buildLeaveTitle(string leaveTitle)
+        {
+            this.leaveTitle = leaveTitle;
+            return this;
+            
+        }
+        public LeaveBuilder buildDescription(string description)
+        {
+            this.description = description;
+            return this;
+        }
+        public LeaveBuilder buildDateModified(DateTime dateModified)
+        {
+            this.dateModified = dateModified;
+            return this;
+        }
+        public LeaveBuilder copy(LeaveDTO leaveDto)
+        {
+            this.Id = leaveDto.Id;
+            this.leaveTitle = leaveDto.leaveTitle;
+            this.description = leaveDto.description;
+            this.dateModified = leaveDto.dateModified;
+            return this;
+        }
+        public LeaveDTO build()
+        {
+            return new LeaveDTO(this);
+        }
     }
 }
