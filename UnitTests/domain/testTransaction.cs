4972081f966346a78bca42e1e063642f5bc6889d
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 22:50:21 +0200

    update tested

--- UnitTests/domain/testTransaction.cs
+++ UnitTests/domain/testTransaction.cs
@@ -18,6 +18,15 @@ namespace UnitTests.domain
             Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), transaction.dateModified);
             Assert.AreEqual(200, transaction.amount);
             Assert.AreEqual("123", transaction.code);
+
+            //TEST UPDATE
+            TransactionDTO update = new TransactionDTO.TransactionBuilder()
+                .copy(transaction)
+                .buildCode("444")
+                .build();
+            Assert.AreEqual("444", update.code);
+            Assert.AreEqual(update.amount, transaction.amount);
+            Assert.AreEqual(update.dateModified, transaction.dateModified);
         }
     }
 }
