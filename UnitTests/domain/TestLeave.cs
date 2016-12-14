e0dff7b44c27f4fc5e3835167bde96cf9800e602
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 21:31:23 +0200

    created  leave test

---
+++ UnitTests/domain/TestLeave.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.domain
+{
+    [TestClass]
+    public class TestLeave
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
