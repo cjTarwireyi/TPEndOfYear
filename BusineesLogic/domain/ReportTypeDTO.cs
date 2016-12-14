1d1731b803c6bd8df88fb460dc6f121dabfd60ed
Yongzz <yongz.ngwenduna@gmail.com>
2016-12-14 02:30:28 +0200

    DTO updated

--- BusineesLogic/domain/ReportTypeDTO.cs
+++ BusineesLogic/domain/ReportTypeDTO.cs
@@ -8,5 +8,46 @@ namespace BusineesLogic.domain
 {
     class ReportTypeDTO
     {
+        string reportCategory { get; set; }
+        string peroidForStats { get; set; }
+
+        private ReportTypeDTO() { }
+
+        public ReportTypeDTO (ReportTypeBuilder reportType)
+        {
+            this.reportCategory = reportType.reportCategory;
+            this.peroidForStats = reportType.peroidForStats;
+        }
+
+        public class ReportTypeBuilder
+        {
+            public string reportCategory;
+            public string peroidForStats;
+
+            public ReportTypeBuilder buildReportCategory(string reportCategory)
+            {
+                this.reportCategory = reportCategory;
+                return this;
+            }
+
+            public ReportTypeBuilder buildPeroidLength(string peroidForStats)
+            {
+                this.peroidForStats = peroidForStats;
+                return this;
+            }
+
+            public ReportTypeBuilder copy(ReportTypeDTO reportType)
+            {
+                this.reportCategory = reportType.reportCategory;
+                this.peroidForStats = reportType.peroidForStats;
+                return this;
+            }
+
+
+            public ReportTypeDTO build()
+            {
+                return new ReportTypeDTO(this);
+            }
+        }
     }
 }
