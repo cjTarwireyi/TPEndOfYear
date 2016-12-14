a2881d4e8cadc49e72d161a316e489d071a5c95e
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 21:40:02 +0200

    Dll update

--- UnitTests/domain/TestLeave.cs
+++ UnitTests/domain/TestLeave.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using BusineesLogic.factories;
+using BusineesLogic.domain;
 namespace UnitTests.domain
 {
     [TestClass]
     public class TestLeave
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDTO()
         {
+            //CREATE TEST
+            DateTime date = DateTime.Parse("1/1/1900 12:00:00 AM");
+
+            LeaveDTO leaveDto =LeaveFactory.createLeave("Sick Leave", "sick", date);
+            Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), leaveDto.dateModified);
+            Assert.AreEqual("Sick Leave", leaveDto.leaveTitle);
+            Assert.AreEqual("sick", leaveDto.description);
+ 
+
         }
     }
 }
