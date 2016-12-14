cf92f6e225570b293900e7d48ec5ac5c79532917
cjTarwireyi <codzatazz@gmail.com>
2016-12-15 00:28:35 +0200

    category DTO implemented

--- BusineesLogic/domain/CategoryDTO.cs
+++ BusineesLogic/domain/CategoryDTO.cs
@@ -8,10 +8,55 @@ using System.Web;
 /// </summary>
 public class CategoryDTO
 {
-	public CategoryDTO()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public CategoryDTO(CategoryBuilder builder)
+    {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.dateCreated = builder.dateCreated;
+    }
+    public int id { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public DateTime dateCreated { get; set; }
+
+    public class CategoryBuilder
+    {
+        public int id;
+        public string name;
+        public string description;
+        public DateTime dateCreated;
+        public CategoryBuilder buildId(int id){
+            this.id = id;
+            return this;
+        }
+        public CategoryBuilder buildBrandName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+        public CategoryBuilder buildDescription(string desc)
+        {
+            this.description = desc;
+            return this;
+        }
+        public CategoryBuilder buildDateCreated(DateTime date)
+        {
+            this.dateCreated = date;
+            return this;
+        }
+        public CategoryBuilder copy(CategoryDTO category)
+        {
+            this.id = category.id;
+            this.name = category.name;
+            this.description = category.description;
+            this.dateCreated = category.dateCreated;
+            return this;
+        }
+        public CategoryDTO build()
+        {
+            return new CategoryDTO(this);
+        }
+    }
+
 }
