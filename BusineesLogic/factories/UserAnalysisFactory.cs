d06055b3a5e6c44f1d5e15874bc8b3b5e13f3897
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 22:04:31 +0200

    implemented factory method

--- BusineesLogic/factories/UserAnalysisFactory.cs
+++ BusineesLogic/factories/UserAnalysisFactory.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BusineesLogic.domain;
 namespace BusineesLogic.factories
 {
     public class UserAnalysisFactory
     {
+        public static UserAnalysisDTO createLeave(int userId, string Desc, DateTime dateModified)
+        {
+            return new UserAnalysisDTO.UserAnalysisBuilder()
+
+            .buildUserId(userId)
+            .buildDescription(Desc)
+            .buildDateModified(dateModified)
+            .build();
+
+        }
     }
 }
