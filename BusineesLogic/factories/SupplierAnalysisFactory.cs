9d2e754c02d22372d6c8b4560dac893a70ee9e53
cjTarwireyi <codzatazz@gmail.com>
2016-12-14 23:35:43 +0200

    supplier analysis factory created

---
+++ BusineesLogic/factories/SupplierAnalysisFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusineesLogic.domain;
+
+namespace BusineesLogic.factories
+{
+  public  class SupplierAnalysisFactory
+    {
+      public static SuplierAnalysisDTO create(int supplierId, string Desc, DateTime dateModified)
+      {
+          return new SuplierAnalysisDTO.SupplierAnalysisBuilder()
+
+          .buildSupplierId(supplierId)
+          .buildDescription(Desc)
+          .buildDateModified(dateModified)
+          .build();
+
+      }
+    }
+}
