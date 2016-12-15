6ad1654c90ed402d643ff5f0958413c91821d25a
214068730 <siraajw19@gmail.com>
2016-12-15 21:35:30 +0200

    update

--- BusineesLogic/domain/PaymentDTO.cs
+++ BusineesLogic/domain/PaymentDTO.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace BusineesLogic.domain
 {
-    class PaymentDTO
+    public class PaymentDTO
     {
+        
+        public decimal due { get; set; }
+        private PaymentDTO() { }
+        public PaymentDTO(PaymentBuilder payment)
+        {
+            
+            this.due = payment.due;
+           
+        }
+ 
+        public class PaymentBuilder
+        {
+          
+            public decimal due;
+
+
+          
+
+            public PaymentBuilder buildCalculator(decimal totalAmount, decimal paymentAmount)
+            {
+                this.due = totalAmount - paymentAmount;
+                return this;
+            }
+
+            public PaymentBuilder copy(PaymentDTO payment)
+            {
+                this.due = payment.due;
+               
+                return this;
+            }
+
+            public PaymentDTO build()
+            {
+                return new PaymentDTO(this);
+            }
+        }
     }
 }
---
+++ UnitTests/factories/TestPayment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.domain;
+
+namespace UnitTests.factories
+{
+    [TestClass]
+    public class TestPayment
+    {
+        [TestMethod]
+        public void testMakePayement()
+        {
+            PaymentDTO payment = PaymentFactory.makePayment((decimal)100,(decimal)200);
+            Assert.AreEqual(payment.due,(decimal)100);
+
+        }
+    }
+}
