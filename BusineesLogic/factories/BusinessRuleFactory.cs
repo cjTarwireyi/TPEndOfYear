ef5be3aaa5708dc070935819b5da95d3dbdc539d
214068730 <siraajw19@gmail.com>
2016-12-15 22:03:06 +0200

    update

---
+++ BusineesLogic/factories/BusinessRuleFactory.cs
@@ -0,0 +1,22 @@
+﻿using BusineesLogic.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusineesLogic.factories
+{
+    public class BusinessRuleFactory
+    {
+        public static BusinessRulesDTO createRule(string name,string descrition)
+        {
+            return new BusinessRulesDTO.BusinessRuleBuilder()
+                                    .buildName(name)
+                                    .buildDescription(descrition)
+                                    .buildDate()
+                                    .build();
+                        
+        }
+    }
+}
