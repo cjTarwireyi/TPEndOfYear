e06b2d97497ff49ecc228f6b7560b506d0b20e96
214068730 <siraajw19@gmail.com>
2016-12-16 03:34:34 +0200

    update

---
+++ BusineesLogic/factories/EmployeeAnalysisFactory.cs
@@ -0,0 +1,23 @@
+﻿using BusineesLogic.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusineesLogic.factories
+{
+    public class EmployeeAnalysisFactory
+    {
+        public static EmployeeAnalysisDTO createAnalysis(int employeeID,int daysLate,int daysWorked,int monthlySales)
+        {
+            return new EmployeeAnalysisDTO.EmployeeAnalysisBuilder()
+                                            .buildEmpID(employeeID)
+                                            .buildDaysLate(daysLate)
+                                            .buildDaysWorkedMonth(daysWorked)
+                                            .buildSalesMade(monthlySales)
+                                            .build();
+        }
+
+    }
+}
