d7eecfb90a053c968916e05f507583c2b214e800
cjTarwireyi <codzatazz@gmail.com>
2016-12-18 12:25:35 +0200

    supplier analysis repo and service tested

---
+++ UnitTests/services/TestSupplierAnalysisService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusineesLogic.domain;
+using BusineesLogic.services;
+using BusineesLogic.factories;
+
+namespace UnitTests.services
+{
+    [TestClass]
+    public class TestSupplierAnalysisService
+    {
+        [TestMethod]
+        public void testSupplierAnalysisServiceMethods()
+        {
+            SupplierAnalysisDAO repo = new SupplierAnalysisDAO();
+            //CREATE TEST
+            DateTime date = DateTime.Parse("1/1/1900 12:00:00 AM");
+
+            SuplierAnalysisDTO supplierAnalysis = SupplierAnalysisFactory.create(1, 4, "low supply", date);
+            Assert.AreEqual(DateTime.Parse("1/1/1900 12:00:00 AM"), supplierAnalysis.dateModified);
+            Assert.AreEqual(4, supplierAnalysis.productId);
+            Assert.AreEqual("low supply", supplierAnalysis.description);
+            repo.save(supplierAnalysis);
+            SuplierAnalysisDTO found = repo.getLastReocrd();
+            //TEST UPDATE
+            SuplierAnalysisDTO update = new SuplierAnalysisDTO.SupplierAnalysisBuilder()
+                .copy(found)
+                .buildProductId(5)
+                .build();
+            repo.update(update);
+            Assert.AreEqual(5, repo.getLastReocrd().productId);
+            Assert.AreEqual(update.description, repo.getLastReocrd().description);
+            Assert.AreEqual(update.dateModified, repo.getLastReocrd().dateModified);
+
+            //TEST DELETE
+            repo.delete(found.id);
+            Assert.IsNull(repo.findByID(found.id));
+   
+        }
+    }
+}
